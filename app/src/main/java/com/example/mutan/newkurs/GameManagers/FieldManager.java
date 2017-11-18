43e6e1ebc7b1c9930a47ec3f9b3092d542855621
Nihsola <mutantikuc@gmail.com>
2017-11-18 15:12:37 +0200

    18.11.2017

--- app/src/main/java/com/example/mutan/newkurs/GameManagers/FieldManager.java
+++ app/src/main/java/com/example/mutan/newkurs/GameManagers/FieldManager.java
@@ -1,6 +1,8 @@
 package com.example.mutan.newkurs.GameManagers;
 
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.view.MotionEvent;
 
@@ -9,45 +11,50 @@ import com.example.mutan.newkurs.GameObjects.GameField;
 import com.example.mutan.newkurs.GameObjects.ReturnButton;
 import com.example.mutan.newkurs.GameObjects.SpinButton;
 import com.example.mutan.newkurs.MyInterfaces.GameManager;
+import com.example.mutan.newkurs.R;
 
 
 public class FieldManager implements GameManager{
+    private GameField gameField;
+
+    private ChipManager chipManager;
+    private NumberManager numberManager;
 
     private CancelButton cancelButton;
     private ReturnButton returnButton;
     private SpinButton spinButton;
-    private NumberManager numberManager;
-    private GameField gameField;
 
-    private int selected = -1;
 
     public FieldManager(Resources resources){
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inScaled = false;
+        Bitmap chip = BitmapFactory.decodeResource(resources, R.drawable.casinochip, options);
+
         cancelButton = new CancelButton(resources);
         returnButton = new ReturnButton(resources);
         spinButton = new SpinButton(resources);
         numberManager = new NumberManager();
-
         gameField = new GameField(resources);
+        chipManager = new ChipManager(chip);
     }
 
-    public void setSelected(int s){
-        selected = s;
-    }
 
     @Override
     public void update() {
-        numberManager.setSelected(selected);
+        numberManager.setSelected(chipManager.selectedChip());
         gameField.update();
+        chipManager.update();
     }
 
     @Override
     public void draw(Canvas canvas) {
         gameField.draw(canvas);
-
         cancelButton.draw(canvas);
         returnButton.draw(canvas);
         spinButton.draw(canvas);
         numberManager.draw(canvas);
+        chipManager.draw(canvas);
     }
 
     @Override
@@ -57,6 +64,7 @@ public class FieldManager implements GameManager{
         returnButton.receiveTouch(event);
         spinButton.receiveTouch(event);
         numberManager.receiveTouch(event);
+        chipManager.receiveTouch(event);
 
         int key = event.getAction();
         switch (key) {
--- app/src/main/java/com/example/mutan/newkurs/GameManagers/GameFieldManager.java
+++ app/src/main/java/com/example/mutan/newkurs/GameManagers/GameFieldManager.java
@@ -11,35 +11,24 @@ import com.example.mutan.newkurs.MyInterfaces.GameManager;
 import com.example.mutan.newkurs.R;
 
 public class GameFieldManager implements GameManager {
-
-    private Bitmap chip;
-    private ChipManager chipManager;
     private FieldManager fieldManager;
 
     public GameFieldManager(Resources resources){
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inScaled = false;
-        chip = BitmapFactory.decodeResource(resources, R.drawable.casinochip, options);
         fieldManager = new FieldManager(resources);
-        chipManager = new ChipManager(chip);
     }
 
     @Override
     public void update() {
-        fieldManager.setSelected(chipManager.selectedChip());
-        chipManager.update();
         fieldManager.update();
     }
 
     @Override
     public void draw(Canvas canvas) {
-        chipManager.draw(canvas);
         fieldManager.draw(canvas);
     }
 
     @Override
     public void receiveTouch(MotionEvent event) {
-        chipManager.receiveTouch(event);
         fieldManager.receiveTouch(event);
     }
 }
